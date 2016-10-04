4cf825e565e709fad84338406e72ee1e4a278062
Michael Weinand <michael.weinand@lgiresearch.com>
2016-10-04 11:26:28 -0500

    initial commit

---
+++ NLog.WindowsAzure.Configuration/CloudConfigLayoutRenderer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAzure;
+using NLog.Config;
+using NLog.LayoutRenderers;
+using System.Text;
+
+namespace NLog.WindowsAzure.Configuration
+{
+    [LayoutRenderer("cloudconfig")]
+    public class CloudConfigLayoutRenderer : LayoutRenderer
+    {
+        ///<summary>
+        /// The AppSetting name.
+        ///</summary>
+        [RequiredParameter]
+        [DefaultParameter]
+        public string Name { get; set; }
+
+        ///<summary>
+        /// The default value to render if the AppSetting value is null.
+        ///</summary>
+        public string Default { get; set; }      
+
+
+        /// <summary>
+        /// Renders the specified application setting or default value and appends it to the specified <see cref="StringBuilder" />.
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            if (Name == null)
+            {
+                return;
+            }
+            
+            string value = CloudConfigurationManager.GetSetting(Name);
+            if (value == null && Default != null)
+            {
+                value = Default;
+            }
+
+            if (string.IsNullOrEmpty(value) == false)
+            {
+                builder.Append(value);
+            }
+        }        
+    }
+}
